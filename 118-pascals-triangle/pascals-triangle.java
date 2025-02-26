7386a6e8ada5f3ff4ec6f9466ae6d0a5232b5ebf
Muskan Jahan <muskanriyaz2001@gmail.com>
2025-02-26 21:07:15 +0530

    Time: 1 ms (83.98%) | Memory: 42 MB (58.53%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,20 +1,15 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> t = new ArrayList<>(numRows);
-
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>(i + 1);
-            row.add(1); // First element is always 1
-            
+            row.add(1); 
             for (int j = 1; j < i; j++) {
-                // Compute the sum from the previous row
                 row.add(t.get(i - 1).get(j - 1) + t.get(i - 1).get(j));
             }
-            
-            if (i > 0) row.add(1); // Last element is always 1
+            if (i > 0) row.add(1); 
             t.add(row);
         }
-        
         return t;
     }
 }
