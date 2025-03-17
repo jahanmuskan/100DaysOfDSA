9cfbd5fe12cb0e6da8d787f4112ef0a0a818320b
Muskan Jahan <muskanriyaz2001@gmail.com>
2025-03-17 20:37:37 +0530

    Time: 3 ms (96.17%) | Memory: 56.9 MB (97.58%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,40 +1,12 @@
 class Solution {
     public int maxArea(int[] height) {
-        // int left = 0, right = height.length - 1;
-        // int maxArea = 0;
-        
-        // while (left < right) {
-        //     int minHeight = height[left] < height[right] ? height[left++] : height[right--];
-        //     maxArea = Math.max(maxArea, minHeight * (right - left + 1));
-        // }
-        
-        // return maxArea;
-        // int left = 0, right = height.length - 1;
-        // int maxArea = 0;
-
-        // while (left < right) {
-        //     int h = height[left] < height[right] ? height[left++] : height[right--]; 
-        //     int width = right - left + 1;
-        //     int area = h * width;
-
-        //     if (area > maxArea) {
-        //         maxArea = area;
-        //     }
-        // }
-        // return maxArea;
         int left = 0, right = height.length - 1;
-        int maxArea = 0, h, width, area;
+        int maxArea = 0;
 
         while (left < right) {
-            width = right - left;  
-            
-            if (height[left] < height[right]) {
-                h = height[left++];
-            } else {
-                h = height[right--];
-            }
-
-            area = h * width;
+            int h = height[left] < height[right] ? height[left++] : height[right--]; 
+            int width = right - left + 1;
+            int area = h * width;
 
             if (area > maxArea) {
                 maxArea = area;
